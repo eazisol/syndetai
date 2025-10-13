48575b3b4b49acb91bdcf03c7ef44eaeaba19dbd
rehanEazisols <muhammad_rehan_khan@eazisols.com>
2025-10-13 20:34:26 +0500

    change url

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -42,7 +42,7 @@ const LoginScreen = ({ onLogin }) => {
           toast.success('Magic link sent! Please check your email.');
           setEmail('');
           // Redirect to library page
-          router.push('/library');
+          // router.push('/library');
         }
       } catch (error) {
         console.error('Login error:', error);
